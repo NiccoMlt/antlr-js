6048663406dc90eb86fbb8d35a46bb167a46a27d
Niccolò Maltoni <niccolo.maltoni@studio.unibo.it>
2020-04-16 18:28:20 +0200

    feat: define Antlr.js behavior with HtmlChatListener

---
+++ src/HtmlChatListener.js
@@ -0,0 +1,51 @@
+const antlr4 = require('antlr4/index');
+const ChatLexer = require('./ChatLexer');
+const ChatParser = require('./ChatParser');
+const ChatListener = require('./ChatListener').ChatListener;
+
+HtmlChatListener = function(res) {
+    this.Res = res;
+    ChatListener.call(this); // inherit default listener
+    return this;
+};
+
+// inherit default listener
+HtmlChatListener.prototype = Object.create(ChatListener.prototype);
+HtmlChatListener.prototype.constructor = HtmlChatListener;
+
+// override default listener behavior
+HtmlChatListener.prototype.enterName = function(ctx) {
+    this.Res.write("<strong>");
+};
+
+HtmlChatListener.prototype.exitName = function(ctx) {
+    this.Res.write(ctx.WORD().getText());
+    this.Res.write("</strong> ");
+};
+
+HtmlChatListener.prototype.exitEmoticon = function(ctx) {
+    const emoticon = ctx.getText();
+
+    if(emoticon === ':-)' || emoticon === ':)')
+    {
+        this.Res.write("🙂");
+    }
+
+    if(emoticon === ':-(' || emoticon === ':(')
+    {
+        this.Res.write("🙁");
+    }
+};
+
+HtmlChatListener.prototype.enterCommand = function(ctx) {
+    if(ctx.SAYS() != null)
+        this.Res.write(ctx.SAYS().getText() + ':' + '<p>');
+    if(ctx.SHOUTS() != null)
+        this.Res.write(ctx.SHOUTS().getText() + ':' + '<p style="text-transform: uppercase">');
+};
+
+HtmlChatListener.prototype.exitLine = function(ctx) {
+    this.Res.write("</p>");
+};
+
+exports.HtmlChatListener = HtmlChatListener;
---
+++ src/index.js
@@ -0,0 +1,43 @@
+const http = require('http');
+const antlr4 = require('antlr4/index');
+const ChatLexer = require('./ChatLexer');
+const ChatParser = require('./ChatParser');
+let HtmlChatListener = require('./HtmlChatListener').HtmlChatListener;
+
+http.createServer((req, res) => {
+
+    res.writeHead(200, {
+        'Content-Type': 'text/html',
+    });
+
+    res.write(`
+        <html lang="en">
+            <head>
+                <meta charset="UTF-8"/>
+                <title>HTML chat</title>
+            </head>
+            <body>
+    `);
+
+    /*
+     * you create the stream of chars from the input,
+     * you give it to the lexer and it transforms them in tokens,
+     * that are then interpreted by the parser.
+     */
+    const input = "john SHOUTS: hello @michael /pink/this will work/ :-) n";
+    const chars = new antlr4.InputStream(input);
+    const lexer = new ChatLexer.ChatLexer(chars);
+    const tokens = new antlr4.CommonTokenStream(lexer);
+    const parser = new ChatParser.ChatParser(tokens);
+    parser.buildParseTrees = true; // redundant, since the option already defaults to true
+    const tree = parser.chat(); // set the root node of the tree as a chat rule
+    const htmlChat = new HtmlChatListener(res);
+    antlr4.tree.ParseTreeWalker.DEFAULT.walk(htmlChat, tree);
+
+    res.write(`
+            </body>
+        </html>
+    `);
+    res.end();
+
+}).listen(1337);
